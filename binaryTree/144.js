d823e71f56c4e47bba7515026c4ff489bdbaabb5
bobwen <wzq@xyz.com>
2020-11-04 10:46:01 +0800

    add tree travel

---
+++ binaryTree/144.js
@@ -0,0 +1,28 @@
+const {tree}  = require("./testData.js")
+
+console.log(tree);
+/**
+ * @description:递归版本遍历 
+ * @param {*}
+ * @return {*}
+ */
+var preorderTraversal = function(root) {
+    const list = [];
+    findValue(root, list);
+    console.log(list)
+    return list
+};
+
+const findValue = (node, list) => {
+    if(!node) return
+    const {left, right, val} = node;
+    // 决定遍历方向
+    list.push(val)
+    if(left) {
+        findValue(left, list);
+    }
+    if(right) {
+        findValue(right, list);
+    }
+}
+preorderTraversal(tree);
