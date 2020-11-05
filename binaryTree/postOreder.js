c69b95b79e3ca18e6ec0881423e5bdfdee1f219b
bobwen <wzq@xyz.com>
2020-11-05 22:05:17 +0800

    add dp

--- binaryTree/postOreder.js
+++ binaryTree/postOreder.js
@@ -4,23 +4,42 @@ const travel = (tree) => {
   const list = [];
   if (!tree) return list;
   const stack1 = [tree];
-  const stack2 = [];
+  const nodeStack = [];
   while (stack1.length) {
     const node = stack1.pop();
     // 访问中节点
-    stack2.push(node);
-    // 第二访问节点 
+    nodeStack.push(node);
+    // 第二访问节点
     if (node.left) stack1.push(node.left);
     // 第一访问节点
     if (node.right) stack1.push(node.right);
   }
-  while (stack2.length) {
-    const node = stack2.pop()
+  while (nodeStack.length) {
+    const node = nodeStack.pop();
     list.push(node.val);
   }
   console.log(list);
   return list;
 };
 
-travel(tree);
+const travel2 = (tree) => {
+  const list = [];
+  if (!tree) return list;
+  const stack = [tree];
+  let cur = tree;
+  while (stack.length) {
+    const top = stack[stack.length - 1];
+    if (top.left && top.left !== cur && top.right !== cur) {
+      stack.push(top.left);
+    } else if (top.right != null && top.right != cur) {
+      stack.push(top.right);
+    } else {
+      list.push(stack.pop().val);
+      cur = top;
+    }
+  }
+  console.log(list);
+  return list;
+};
 
+travel2(tree);
--- dp/fib.js
+++ dp/fib.js
@@ -1,15 +1,15 @@
 // f(n) = f(n - 2) + f(n - 1)
+// 【0，1，1】
+
 const fib = (num) => {
-    if ( num < 3) return num;
-  let first = 1;
-  let second = 2;
-  let n = 0;
-  for (let i = 2; i < num; i ++) {
-      n = first + second;
-      first = second;
-      second = n;
+  const memo = [0,1,1]
+  let a = 1;
+  if ( num < 3) return num;
+  for (let i = 3; i <= num; i ++) {
+      memo[i] =memo[i -1] + memo[i -2]  
   }
-  return n;
+  console.log(memo)
+  return memo[num];
 };
 
 console.log(fib(4));
